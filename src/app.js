ea8021d8560a9aeee1ee11f23b7edc1d0829efc9
yasmimc <yasmimcavalcanti.r@gmail.com>
2021-12-03 23:15:31 -0300

    Remove GET /health route

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
 import express from 'express';
 import cors from 'cors';
-import routes from './routes.js';
 import recommendationRoutes from './routers/recommendationRouter.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(routes);
 app.use(recommendationRoutes);
 
 export default app;
